ab25bc0bbdeae19bd4a9828fdd2406d9736ac3f0
Jia Yee <jiayeerawr@gmail.com>
2016-07-22 15:22:57 +0800

    Add insertion sort
    Unit test succeeded

--- sort.c
+++ sort.c
@@ -14,24 +14,27 @@ int main() {
     }
     // bubbleSort(arr, number);
     // selectSort(arr, number);
-    // insertSort(arr, number);
+    insertSort(arr, number);
     return 0;
 }
 
 void bubbleSort(int a[], int n) {
-    int i, j;
+    int i, j, swapped;
     for (i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
     for (i = 0; i < n; i++) {
+        swapped = 0;
         for (j = 0; j < n-i; j++) {
             if (a[j] > a[j+1]) {
                 int temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
+                swapped = 1;
             }
         }
+        if (!swapped) break;
     }
     for (i = 0; i < n; i++) {
         printf("%d ", a[i]);
@@ -67,7 +70,14 @@ void insertSort(int a[], int n) {
     }
     printf("\n");
     for (i = 1; i < n; i++) {
-        ;
+        int j = i-1, k = i;
+        while (j >= 0 && a[j] > a[k]) {
+            int temp = a[k];
+            a[k] = a[j];
+            a[j] = temp;
+            j--;
+            k--;
+        }
     }
     for (i = 0; i < n; i++) {
         printf("%d ", a[i]);
